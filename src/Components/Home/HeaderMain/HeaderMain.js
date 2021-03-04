e2307864af23be486099399d7dba0edb572705dc
UtshoBarua <utsho45-211@diu.edu.bd>
2021-03-05 00:19:16 +0600

    hot-onion

---
+++ src/Components/Home/HeaderMain/HeaderMain.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../Shared/Navbar/Navbar';
+import './'
+const HeaderMain = () => {
+    return (
+        <div className="Header">
+           <h1>Hello its Header</h1>
+        </div>
+    );
+};
+
+export default HeaderMain;
---
+++ src/Components/Home/HomeMain/HomeMain.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Shared/Navbar/Navbar';
+import HeaderMain from '../HeaderMain/HeaderMain.js';
+
+const HomeMain = () => {
+    return (
+        <div>
+            <Navbar/>
+            <HeaderMain/>
+        </div>
+    );
+};
+
+export default HomeMain;
