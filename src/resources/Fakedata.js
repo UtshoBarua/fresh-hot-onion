2329bd1b5a453e666ff7bfdcfe25fa1a6a9291c6
UtshoBarua <utsho45-211@diu.edu.bd>
2021-03-05 15:22:20 +0600

    after-header

---
+++ src/resources/Fakedata.js
@@ -0,0 +1,40 @@
+
+import Lunch from "../resources/lunch/lunch1.png"
+import Lunch2 from "../resources/lunch/lunch2.png"
+import Lunch3 from "../resources/lunch/lunch2.pn
+
+export const Lunch = [
+    {
+        "id": 1,
+        "first_name": "Bond",
+        "last_name": "Bond Bowery",
+        "picture": Lunch
+      }, {
+        "id": 2,
+        "first_name": "Elnar",
+        "last_name": "Elnar Tomaskov",
+        "picture": Lunch2
+      }, {
+        "id": 3,
+        "first_name": "Rodd",
+        "last_name": "Rodd Toby",
+        "picture": Lunch3
+      }, {
+        "id": 4,
+        "first_name": "Ermentrude",
+        "last_name": "Ermentrude Maplesden",
+        "picture": Lunch
+      }, {
+        "id": 5,
+        "first_name": "Mabelle",
+        "last_name": "Mabelle Count",
+        "picture":Lunch2
+      }, {
+        "id": 6,
+        "first_name": "Colene",
+        "last_name": "Colene Brian",
+        "picture": Lunch3
+      
+    }
+];
+export const Dinner = 
